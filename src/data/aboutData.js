c2df23cd6a225f57d63e84f8893ceee16c44a516
Monica Priya <100186167+Monica1310@users.noreply.github.com>
2022-06-18 00:22:52 +0530

    update

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: `I'm an ambitious Full Stack Web Developer with a specialization in MERN stack, to have a growth-oriented and challenging career, where I can contribute my knowledge and skills to the organization and enhance my experience through continuous learning and teamwork.. `,
+    description1: `An ambitious Full Stack Web Developer with a specialization in MERN stack, to have a growth-oriented and challenging career, where I can contribute my knowledge and skills to the organization and enhance my experience through continuous learning and teamwork.. `,
     description2: "",
     image: 2
 }

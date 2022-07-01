dcb27538370a3f87dfb19335ef73f3d4019a2490
Monica Priya <100186167+Monica1310@users.noreply.github.com>
2022-07-01 19:24:21 +0530

    comit

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'R Monica Priya',
     title: "Hello! I'am",
     // desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://media-exp2.licdn.com/dms/image/C4D03AQHB6EhhmoTgwQ/profile-displayphoto-shrink_800_800/0/1655489723396?e=1660780800&v=beta&t=LdbVEH2V_UTrkm8gvXYy89eP5CYi3b8y0CgJNneeljA',
+    image: 'https://media-exp2.licdn.com/dms/image/C4D03AQGQs1USPeiGVQ/profile-displayphoto-shrink_800_800/0/1655492717745?e=1661990400&v=beta&t=KvhPehugb0cuEor1Pygam3FZZYE4gx76sEH7p-Qtaxk',
     resumePdf: resume
 }
